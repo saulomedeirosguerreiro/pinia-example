dc42673790ff872713a9e16937af93b5e976e50a
saulomedeirosguerreiro <saulo.medeiros.guerreiro@gmail.com>
2023-08-16 19:55:24 +0100

    example finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
+import { useProductStore } from "@/stores/ProductStore";
+import { useCartStore } from "@/stores/CartStore";
+import AppButton from "@/components/AppButton.vue";
+const productStore = useProductStore();
+const cartStore = useCartStore();
+
+cartStore.$onAction(({ name, args, after, onError }) => {
+  if (name === "addItems") {
+    after(() => {
+      console.log(args[0]);
+    });
+    onError((error) => {
+      console.log("Hello error: ", error.message);
+    });
+  }
+});
+productStore.fill();
 </script>
 
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="cartStore.undo">Undo</AppButton>
+      <AppButton class="ml-2" @click="cartStore.redo">Redo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
+        @addToCart="cartStore.addItems($event, product)"
       />
     </ul>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createPinia } from "pinia";
 
 // Icons and Styles
 import FontAwesomePlugin from "./plugins/FontAwesome";
@@ -9,9 +10,13 @@ import "./assets/main.pcss";
 import AppButton from "./components/AppButton.vue";
 import AppCountInput from "./components/AppCountInput.vue";
 import AppModalOverlay from "./components/AppModalOverlay.vue";
+import { PiniaHistoryPlugin } from "@/plugins/PiniaHistoryPlugin";
 
+const pinia = createPinia();
+pinia.use(PiniaHistoryPlugin);
 // Init App
 createApp(App)
+  .use(pinia)
   .use(FontAwesomePlugin)
   .component("AppButton", AppButton)
   .component("AppCountInput", AppCountInput)
